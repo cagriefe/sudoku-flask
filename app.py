31d2c2a3ab7d4b8a58e5839abf24989def8ea3a0
cagriefe <cagriefe34@gmail.com>
2024-06-18 17:47:51 +0300

    interactive with user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from game.generator import generate_sudoku
 
 app = Flask(__name__)
@@ -6,6 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     board = generate_sudoku('expert')
+    return render_template('index.html', board=board)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    board = []
+    for i in range(9):
+        row = []
+        for j in range(9):
+            cell_value = request.form.get(f'cell-{i}-{j}')
+            if cell_value == '' or cell_value is None:
+                row.append("")
+            else:
+                row.append(int(cell_value))
+        board.append(row)
+    
+    # Process the board as needed (e.g., validate it, solve it, etc.)
+    # For now, just print the board to the console
+    print(board)
+    
     return render_template('index.html', board=board)
 
 if __name__ == '__main__':
