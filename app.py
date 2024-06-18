3d919307141e6ff4df19db1caed44249e0c32851
cagriefe <cagriefe34@gmail.com>
2024-06-18 17:38:00 +0300

    difficulty choice added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    board = generate_sudoku()
+    board = generate_sudoku('expert')
     return render_template('index.html', board=board)
 
 if __name__ == '__main__':
--- game/generator.py
+++ game/generator.py
@@ -7,22 +7,33 @@ import numpy as np
 #%20-%30 in expert
 
 
-def generate_sudoku():
+def generate_sudoku(difficulty='easy'):
     grid = [[random.randint(1, 9) for _ in range(9)] for _ in range(9)]
     
     total_cells = 9 * 9
-    num_zeros = int(total_cells * 0.55)
+    if difficulty == 'easy':
+        zero_percentage = 0.45
+    elif difficulty == 'hard':
+        zero_percentage = 0.60
+    elif difficulty == 'expert':
+        zero_percentage = 0.75
+    else:
+        raise ValueError("Invalid difficulty level. Choose from 'easy', 'hard', or 'expert'.")
+    
+    num_zeros = int(total_cells * zero_percentage)
     positions = list(range(total_cells))
     random.shuffle(positions)
     
     for i in range(num_zeros):
         row = positions[i] // 9
         col = positions[i] % 9
-        grid[row][col] = 0
+        grid[row][col] = ""
     
     return grid
 
-random_grid = generate_sudoku()
+# Generate a Sudoku grid with 'easy' difficulty
+random_grid = generate_sudoku('easy')
 
+# Print the grid
 for row in random_grid:
     print(row)
